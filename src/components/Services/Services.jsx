77f47e238d8405db0f378f36a9dc77c17d2b2911
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-03-02 10:04:13 +0530

    Portfolio Services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,8 +1,72 @@
 import React from "react";
 import "./Services.css";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  const services = [
+    {
+      id: `title-${Math.random()}`,
+      title: "UI/UX Design",
+      list: [
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+      ],
+    },
+    {
+      id: `title-${Math.random()}`,
+      title: "Web Development",
+      list: [
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+      ],
+    },
+    {
+      id: `title-${Math.random()}`,
+      title: "Content Creation",
+      list: [
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+        { id: `list-${Math.random()}`, note: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+      ],
+    },
+  ];
+
+  return (
+    <section id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div className='container service-container'>
+        {services.map((se) => {
+          return (
+            <article className='service' key={se.id}>
+              <div className='service-head'>
+                <h3>{se.title}</h3>
+              </div>
+              <ul className='service-list'>
+                {se.list.map((lis) => {
+                  return (
+                    <li key={lis.id}>
+                      <AiOutlineCheckCircle className='service-list-icon' />
+                      <p>{lis.note}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
